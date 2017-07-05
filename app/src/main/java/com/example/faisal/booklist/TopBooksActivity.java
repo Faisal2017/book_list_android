f3783d731747e839ed45de429cf6c3e70e284c90
Faisal <faisal_a@live.co.uk>
2017-07-05 21:41:48 +0100

    added activity and adapter files

--- app/src/main/java/com/example/faisal/booklist/TopBooksActivity.java
+++ app/src/main/java/com/example/faisal/booklist/TopBooksActivity.java
@@ -2,6 +2,9 @@ package com.example.faisal.booklist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.view.View;
+import java.util.ArrayList;
 
 public class TopBooksActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class TopBooksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.books_list);
+
+        TopBooks topBooks = new TopBooks();
+
+        ArrayList<Book> list = topBooks.getList();
+
+        TopBooksAdapter bookAdapter = new TopBooksAdapter(this, list);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(bookAdapter);
     }
 }
---
+++ app/src/main/java/com/example/faisal/booklist/TopBooksAdapter.java
@@ -0,0 +1,46 @@
+package com.example.faisal.booklist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Faisal on 05/07/2017.
+ */
+
+public class TopBooksAdapter extends ArrayAdapter<Book> {
+
+    public TopBooksAdapter(Context context, ArrayList<Book> list) {
+
+        super(context, 0, list);
+
+        @Override
+        public View getView(int position, View listItemView, ViewGroup parent) {
+
+            if (listItemView == null) {
+                listItemView = LayoutInflater.from(getContext()).inflate(R.layout.books_item, parent, false);
+            }
+
+            Book currentBook = getItem(position);
+
+            TextView ranking = (TextView) listItemView.findViewById(R.id.ranking);
+            ranking.setText(currentBook.getRanking().toString());
+
+            TextView name = (TextView) listItemView.findViewById(R.id.name);
+            name.setText(currentBook.getName().toString());
+
+            TextView author = (TextView) listItemView.findViewById(R.id.author);
+            author.setText(currentBook.getAuthor().toString());
+
+            listItemView.setTag(currentBook);
+
+            return listItemView;
+        }
+
+
+}
